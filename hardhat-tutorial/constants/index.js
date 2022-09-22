a6321622d61f792d2593a7f00f783c0504a737ca
Elicia Au Duong <eliciaauduong@outlook.com>
2022-09-22 19:35:14 +1000

    created nft dapp

---
+++ hardhat-tutorial/constants/index.js
@@ -0,0 +1,6 @@
+// Address of the Whitelist Contract that you deployed
+const WHITELIST_CONTRACT_ADDRESS = "0x1596F0AAbc938eD5622b6EBAdcfE122F3e93e10d";
+// URL to extract Metadata for a Crypto Dev NFT
+const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
